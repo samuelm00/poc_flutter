3bba1de4f1d5b44067dba5925079558a0a755066
samuelm00 <sa.mitterrutzner@gmail.com>
2023-02-10 13:10:43 +0100

    add responsive layout builder to abstract breakpoints away from rest of the application

---
+++ lib/config/layout/breakpoints.dart
@@ -0,0 +1,11 @@
+/// Namespace for Default Dog Owner App Breakpoints
+abstract class AppBreakpoints {
+  /// Max width for a small layout.
+  static const double small = 380;
+
+  /// Max width for a large layout.
+  static const double medium = 426;
+
+  /// Max width for a large layout.
+  static const double large = 480;
+}
